32a8a4047ef9c0ba24aa4ef1f18e2a92425e6942
Habiba ELkabbany <helkabbany2@gmail.com>
2025-02-28 23:15:15 +0200

    Adjustment to Order Model

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.repository;
 
+import com.example.model.Order;
 import com.example.model.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -37,9 +39,38 @@ public class UserRepository extends MainRepository<User>{
         save(user);
         return user;
     }
-//    public List<Order> getOrdersByUserId(UUID userId);
-//    public void addOrderToUser(UUID userId, Order order);
-//    public void removeOrderFromUser(UUID userId, UUID orderId);
+    public List<Order> getOrdersByUserId(UUID userId){
+        User user = getUserById(userId);
+
+        return user.getOrders();
+    }
+    public void addOrderToUser(UUID userId, Order order){
+        ArrayList<User> users = getUsers();
+        for(User user : users){
+            if(user.getId().equals(userId)) {
+                user.getOrders().add(order);
+                overrideData(users);
+                System.out.println("Order added");
+                return;
+            }
+        }
+        System.out.println("Order not added: User not found");
+    }
+    public void removeOrderFromUser(UUID userId, UUID orderId){
+        ArrayList<User> users = getUsers();
+        for(User user : users){
+            if(user.getId().equals(userId)) {
+                List<Order> orders = user.getOrders();
+                for(Order order : orders){
+                    if (order.)
+                }
+                overrideData(users);
+                System.out.println("Order added");
+                return;
+            }
+        }
+        System.out.println("Order not added: User not found");
+    }
     public void deleteUserById(UUID userId){
         ArrayList<User> users = getUsers();
         for(User user : users){
