f86579635441b608b7000d52f996f859551ed3d1
Habiba ELkabbany <helkabbany2@gmail.com>
2025-03-09 22:01:06 +0200

    Merging

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,4 +1,78 @@
 package com.example.controller;
 
 public class UserController {
+<<<<<<< Updated upstream
+=======
+//The Dependency Injection Variables
+//The Constructor with the requried variables mapping the Dependency Injection.
+    private final UserService userService;
+    private final CartService cartService;
+    private final ProductService productService;
+    public UserController(UserService userService,CartService cartService,ProductService productService) {
+        this.userService = userService;
+        this.cartService = cartService;
+        this.productService = productService;
+    }
+    @PostMapping("/")
+    public User addUser(@RequestBody User user){
+          return  userService.addUser(user);
+    }
+
+    @GetMapping("/")
+    public ArrayList<User> getUsers(){
+       return userService.getUsers();
+    }
+    
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable UUID userId){
+        return userService.getUserById(userId);
+    }
+    @GetMapping("/{userId}/orders")
+    public List<Order> getOrdersByUserId(@PathVariable UUID userId){
+         return userService.getOrdersByUserId(userId);
+    }
+    @PostMapping("/{userId}/checkout")
+    public String addOrderToUser(@PathVariable UUID userId){
+         userService.addOrderToUser(userId);
+         return "Order added";
+    }
+    @PostMapping("/{userId}/removeOrder")
+    public String removeOrderFromUser(@PathVariable UUID userId, @RequestParam UUID orderId){
+        userService.removeOrderFromUser(userId, orderId);
+        return "Order removed";
+    }
+    @DeleteMapping("/{userId}/emptyCart")
+    public String emptyCart(@PathVariable UUID userId){
+        userService.emptyCart(userId);
+        return "Cart empty";
+    }
+    @PutMapping("/addProductToCart")
+    public String addProductToCart(@RequestParam UUID userId, @RequestParam UUID productId){
+        Cart cart = cartService.getCartByUserId( userId);
+        UUID cartId = cart.getId();
+        Product product = productService.getProductById(productId);
+
+        cartService.addProductToCart(cartId, product);
+        return "Product added";
+    }
+    @PutMapping("/deleteProductFromCart")
+    public String deleteProductFromCart(@RequestParam UUID userId, @RequestParam UUID productId){
+        Cart cart = cartService.getCartByUserId(userId);
+        UUID cartId = cart.getId();
+        Product product = productService.getProductById(productId);
+
+        cartService.deleteProductFromCart(cartId, product);
+        return "Product added";
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public String deleteUserById(@PathVariable UUID userId){
+        userService.deleteUserById(userId);
+        return "User deleted";
+    }
+
+
+
+
+>>>>>>> Stashed changes
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,4 +1,104 @@
 package com.example.service;
 
+<<<<<<< Updated upstream
 public class UserService {
+=======
+import com.example.model.Cart;
+import com.example.model.Order;
+import com.example.model.Product;
+import com.example.model.User;
+
+import com.example.repository.UserRepository;
+import com.example.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@SuppressWarnings("rawtypes")
+public class UserService extends MainService<User>{
+    UserRepository userRepository;
+    CartService cartService;
+    @Autowired
+    public UserService(UserRepository userRepository, CartService cartService) {
+        this.userRepository = userRepository;
+        this.cartService = cartService;
+    }
+    public User addUser(User user) {
+        return userRepository.addUser(user);
+    }
+    public ArrayList<User> getUsers() {
+        return userRepository.getUsers();
+    }
+    public User getUserById(UUID userId){
+        User user = userRepository.getUserById(userId);
+        if(user == null){
+            throw new RuntimeException("User not found");
+        }
+        else {
+            return user;
+        }
+    }
+    public List<Order> getOrdersByUserId(UUID userId){
+//        return userRepository.getOrdersByUserId(userId);
+        List<Order> orders = userRepository.getOrdersByUserId(userId);
+        if(orders == null){
+            throw new RuntimeException("User with Id " + userId+ " not found");
+        }
+        if (orders.isEmpty()) {
+            System.out.println("User has no orders yet");
+        }
+        return orders;
+    }
+    public void deleteUserById(UUID userId){
+        userRepository.deleteUserById(userId);
+    }
+    public void removeOrderFromUser(UUID userId, UUID orderId){
+        userRepository.removeOrderFromUser(userId, orderId);
+    }
+    public void emptyCart(UUID userId) {
+        Cart cart = cartService.getCartByUserId(userId);
+        if (cart == null) {
+            throw new RuntimeException("Cart not found");
+        }
+        UUID cartId = cart.getId();
+        List<Product> products = cart.getProducts();
+        if (products.isEmpty()) {
+            System.out.println("Cart is empty");
+            return;
+        }
+        for (Product product : products) {
+            cartService.deleteProductFromCart(cartId, product);
+        }
+        System.out.println("Cart emptied");
+    }
+    public void addOrderToUser(UUID userId){
+        Cart cart = cartService.getCartByUserId(userId);
+        if (cart == null) {
+            throw new RuntimeException("Cart not found");
+        }
+        UUID cartId = cart.getId();
+        List<Product> products = cart.getProducts();
+        if (products.isEmpty()) {
+            System.out.println("Cart is empty");
+            return;
+        }
+        double totalPrice = 0;
+        for (Product product : products) {
+            totalPrice += product.getPrice();
+        }
+        emptyCart(userId);
+
+
+        Order order = new Order(userId,totalPrice,products);
+        userRepository.addOrderToUser(userId, order);
+    }
+
+
+
+
+>>>>>>> Stashed changes
 }
