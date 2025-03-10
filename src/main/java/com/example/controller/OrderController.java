4244a3e8a0da63bd64636b635ef670e62c7c3585
Omar Ramadan <73427948+RamadanO007@users.noreply.github.com>
2025-03-10 06:15:06 +0200

    Omar Commit1 isa kolo tamam w raben yestor 3al branches

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,4 +1,58 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.example.service.ProductService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Order;
+
+import com.example.service.OrderService;
+
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+    //The Dependency Injection Variables
+//The Constructor with the requried variables mapping the Dependency Injection.
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/")
+    public void addOrder(@RequestBody Order order){
+        orderService.addOrder(order);
+    }
+
+    @GetMapping("/{orderId}")
+    public Order getOrderById(@PathVariable UUID orderId){
+        return orderService.getOrderById(orderId);
+    }
+
+    @GetMapping("/")
+    public ArrayList<Order> getOrders(){
+        return orderService.getOrders();
+    }
+
+    @DeleteMapping("/delete/{orderId}")
+    public String deleteOrderById(@PathVariable UUID orderId){
+        return deleteOrderById(orderId);
+    }
+
+
+
+
+
+
 }
+
